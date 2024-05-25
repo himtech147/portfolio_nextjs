c03cda80bb823b60f8de48686580288bb9d313cc
Himanshu <himtech147@gmail.com>
2024-05-26 00:35:56 +0530

    Hero Section Done

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import Hero from "@/components/Hero";
+import { FloatingNav } from "@/components/ui/FloatingNavbarr";
 import Image from "next/image";
+import { FaHome } from "react-icons/fa";
 
 export default function Home() {
   return (
@@ -8,6 +10,15 @@ export default function Home() {
     items-center overflow-hidden mx-auto sm:px-10 px-5"
     >
       <div className="max- w-7xl w-full">
+        <FloatingNav
+          navItems={[
+            {
+              name: "Home",
+              link: "/",
+              icon: <FaHome />,
+            },
+          ]}
+        />
         <Hero />
       </div>
     </main>
